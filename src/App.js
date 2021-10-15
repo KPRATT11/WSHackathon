8a021523c3ba321cc4bc4747e66ac6a56f84b223
Keegan Pratt <pratt.keegan@gmail.com>
2021-10-16 08:05:10 +1100

    init State for app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import ButtonWrapper from './components/ButtonWrapper';
 
 function App() {
 
-
-
   const [room, setRoom] = useState(0)
+  const [waterLevel, setWaterLevel] = useState(0) //init to defualt water level
+  const [timePassed, setTimePassed] = useState(0) //set to miliseconds passed can format it use the Date class
+  const [rainAmount, setRainAmount] = useState(0)
+  const [rainCycles, setRainCycles] = useState(0) //amount of times it has rained
 
   const displayRoom = () => {
     //probs just have a switch here that returns the room component
