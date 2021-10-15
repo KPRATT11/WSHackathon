9f1468ac4b2d411ead1eb509e70100ed2103dbee
Keegan Pratt <pratt.keegan@gmail.com>
2021-10-16 07:54:51 +1100

    added the buttons

--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -3,8 +3,15 @@ import React, { useState } from 'react';
 const ButtonWrapper = () => {
     const [showing, setShowing] = useState(true)
 
+    const toggleShow = () => {
+        setShowing(!showing)
+    }
+
     return ( 
-        <div className="ButtonWrapper">
+        <div 
+            className="ButtonWrapper"
+            onClick={toggleShow}
+            >
             <button className="showHide">Show/Hide</button>
             {showing &&             
                 <div className="roomButtons">
