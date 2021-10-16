e320d609b331d9667aaaf2615cce86c9f48c4611
sarahandreas <sarah.andreas@gmail.com>
2021-10-16 12:42:16 +1100

    duckygit add .

--- src/components/rooms/Kitchen.js
+++ src/components/rooms/Kitchen.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import './Kitchen.css'
 import Timer from '../Timer';
+import kitchenPlain from '../../svgs/kitchenPlain.jpg'
+import water from '../../svgs/sinkoverflow.png'
+import kitchenHazard from '../../svgs/kitchenHazard.jpg'
 
 export default function Kitchen(props) {
+
+    const [waterRunning, setWaterRunning] = useState(true)
     
     const [timeLeft, setTimeLeft] = useState(props.initTime)
 
@@ -11,9 +17,37 @@ export default function Kitchen(props) {
         }, 1000)
     }, [timeLeft])
 
+    let kitchenType = waterRunning ? kitchenHazard : kitchenPlain
+
+    let message
+    if (waterRunning && timeLeft) {
+        message = 'Stop the Tap!!!'
+    } else if (waterRunning && !timeLeft) {
+        message = 'You ran out of time!'
+    } else {
+        console.log(timeLeft, waterRunning)
+        message = 'Good Work! You saved Water!'
+    }
+    
     return (
-        <div className="kitchen">
-            <p>hello</p>
+        <div 
+            className="kitchen"
+            >
+            <h1>{message}</h1>
+            <div className="imgs">
+                <img 
+                    src={kitchenType} 
+                    alt="" 
+                    className="background"
+                    />
+                <img 
+                    src={water} 
+                    alt=""
+                    className="water" 
+                    hidden={!waterRunning}
+                    onClick={() => setWaterRunning(false)}
+                    />
+            </div>
             <Timer time={timeLeft}/>
         </div>
     )
